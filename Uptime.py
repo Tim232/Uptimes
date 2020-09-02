4afd5e99e8f6667250150f3de0f8476ca88b31ef
Tim232 <64291996+Tim232@users.noreply.github.com>
2020-09-03 00:09:18 +0900

    Create Uptime.py

---
+++ Uptime.py
@@ -0,0 +1,14 @@
+import datetime
+
+class Uptime:
+    def __init__(self):
+        self.start_time = datetime.datetime.now()  # Timestamp when it initializes.
+
+    def uptime(self):
+        now = datetime.datetime.now()  # Timestamp when uptime function runs.
+        delta = now - self.start_time
+        hours, remainder = divmod(int(delta.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+        uptime_stamp = f"**{days}** days **{hours}** hours **{minutes}** minutes **{seconds}** seconds"
+        return uptime_stamp
