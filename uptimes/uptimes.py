59fa080bc5440208aab206507dac72607d634b1f
Decave <decave27@gmail.com>
2021-03-28 20:32:28 +0900

    refactor: add Uptime class, repr, property

--- uptimes/uptimes.py
+++ uptimes/uptimes.py
@@ -1,24 +1,34 @@
 import datetime
 
-start_time = datetime.datetime.now()  # Timestamp when it initializes.
-
 
 def __init__():
-    start_time = datetime.datetime.now()  # Timestamp when it initializes.
+    Uptime = Uptime()
 
 
-def uptimes():
-    stamp = raw_uptimes()
-    uptime_stamp = (
-        f"**{stamp['days']}** days **{stamp['hours']}** hours **{stamp['minutes']}** minutes **{stamp['seconds']}** seconds"
-    )
-    return uptime_stamp
-
-def raw_uptimes():
-    now = datetime.datetime.now()  # Timestamp when uptime function runs.
-    delta = now - start_time
-    hours, remainder = divmod(int(delta.total_seconds()), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    days, hours = divmod(hours, 24)
-    uptime_stamp = {"days":days,"hours":hours,"minutes":minutes,"seconds":seconds}
-    return uptime_stamp
+class Uptime:
+    def __init__(self, start=None):
+        self.start_time = start or datetime.datetime.now()
+    
+    def __repr__(self):
+        return f"<Uptime {self.raw['days']} Days {self.raw['hours']} Hours {self.raw['minutes']} Minutes {self.raw['seconds']} Seconds>"
+    
+    @property
+    def raw(self):
+        now = datetime.datetime.now()  # Timestamp when uptime function runs.
+        delta = now - self.start_time
+        hours, remainder = divmod(int(delta.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+        uptime_stamp = {
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds,
+        }
+        return uptime_stamp
+    
+    @property
+    def uptimes(self):
+        stamp = self.raw()
+        uptime_stamp = f"**{stamp['days']}** days **{stamp['hours']}** hours **{stamp['minutes']}** minutes **{stamp['seconds']}** seconds"
+        return uptime_stamp
